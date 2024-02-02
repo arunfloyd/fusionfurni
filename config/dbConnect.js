c6a807a7163be2839bd53c5e7e285fedcc81488b
ARUN VINOD K <78198360+arunfloyd@users.noreply.github.com>
2024-02-02 20:12:40 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
-const dbConnect = () => {
+
+const dbConnect = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGODB_URI);
-  } catch (err) {}
+    const conn = await mongoose.connect(
+      "mongodb+srv://arunfloyd9497:tLgORcKeaQOvIV0v@fusion.ujatfll.mongodb.net/furniture",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    console.log("MongoDB Connected");
+    // Additional setup or logging can be done here if needed.
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+  }
 };
+
 module.exports = dbConnect;
